337ec9c66f3cf06b6d3e6b65202c49e8e94fdc01
YaYeet33 <159903209+YaYeet33@users.noreply.github.com>
2024-06-18 21:35:38 +1000

    Update init.py

--- init.py
+++ init.py
@@ -1,11 +1,31 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
+from flask_session import Session
 
 app = Flask(__name__)
 
 # Configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SECRET_KEY'] = '123456'
 
 # Initialize the database
 db = SQLAlchemy(app)
+
+# Initialize the session
+Session(app)
+
+migrate = Migrate(app, db)
+
+import models
+
+# Create the database tables if they do not exist
+with app.app_context():
+    db.create_all()
+
+
+
+
+
