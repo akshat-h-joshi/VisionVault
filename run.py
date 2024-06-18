3f251c4a6efff0334885818f1abd4aaccbf75a8b
YaYeet33 <159903209+YaYeet33@users.noreply.github.com>
2024-06-18 21:35:52 +1000

    Update run.py

--- run.py
+++ run.py
@@ -3,17 +3,6 @@ from init import app, db
 from models import User, Goal, Task
 from flask_session import Session
 
-# Configure the session
-app.config['SESSION_TYPE'] = 'filesystem'
-app.config['SECRET_KEY'] = '123456'
-
-# Initialize the session
-Session(app)
-
-# Create the database tables if they do not exist
-with app.app_context():
-    db.create_all()
-
 # Define the main entry point of the application
 if __name__ == '__main__':
     app.run(debug=True)
